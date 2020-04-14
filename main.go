67675f32b42e782f5f353fcaf202872ef6cfcf4f
lamthanhhai141@gmail.com <lamthanhhai141@gmail.com>
2020-04-14 23:22:49 +0700

    add

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"os"
+)
 
+// Log linter
+var Log *Logging
+
+func main() {
+	Log = newLogging()
+	// logging = newLogger()
+}
+
+// Error export error log
+func Error(v ...interface{}) {
+	Log.ERROR(v...)
+}
+
+// Info export none error log
+func Info(v ...interface{}) {
+	Log.INFO(v...)
+}
+
+// Fatal linter
+func Fatal(v ...interface{}) {
+	Log.ERROR(v...)
+	os.Exit(1)
 }
