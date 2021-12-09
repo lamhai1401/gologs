b50ddac4b9831c0f45e30947ccc92bab5bdeaf6d
lamhai1401 <lamthanhhai141@gmail.com>
2021-12-09 17:34:53 +0700

    add off log

--- logs/init.go
+++ logs/init.go
@@ -8,35 +8,46 @@ import (
 
 // Log linter
 var Log logger.Log
+var OffLog string
 
 func init() {
 	Log = logger.NewFactorLog()
 	// logging = newLogger()
+	OffLog = os.Getenv("OFF_LOG")
 }
 
 // Error export error log
 func Error(v ...interface{}) {
-	Log.ERROR(v...)
+	if OffLog != "1" {
+		Log.ERROR(v...)
+	}
 }
 
 // Info export none error log
 func Info(v ...interface{}) {
-	Log.INFO(v...)
+	if OffLog != "1" {
+		Log.INFO(v...)
+	}
 }
 
 // Debug export none error log
 func Debug(v ...interface{}) {
-	if os.Getenv("DEBUG") == "1" {
+	if os.Getenv("DEBUG") == "1" && OffLog != "1" {
 		Log.DEBUG(v...)
 	}
 }
 
 // Warn export none error log
 func Warn(v ...interface{}) {
-	Log.WARN(v...)
+	if OffLog != "1" {
+		Log.WARN(v...)
+	}
 }
 
 // Stack linter
 func Stack(v ...string) {
+	// if OffLog != "1" {
+	// 	Log.STACK(v...)
+	// }
 	Log.STACK(v...)
 }
